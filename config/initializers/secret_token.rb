d968fc477558bb3d0aa269df5b17594fe0bc3b5d
Kiyoto Shinomura <k-shinomura@kddi.com>
2013-03-24 08:53:41 +0900

    firtst commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oboeita::Application.config.secret_token = '7a84fff8f999a5aee247eb038473ed11d8f5de6c2778f75d03f98afa6238804f69854b22f73e6354defd60b920e52bd9c92da0d17d8f4f0cddff4367a9c69138'
